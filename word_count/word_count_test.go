7c0cbdd2dcda6ee63cece8594d1d1c4f82cad65d
Christian Mc Carthy <cmccarthy@pivotal.io>
2019-02-05 16:56:52 +0000

    Create test for WordCount

--- word_count/word_count_test.go
+++ word_count/word_count_test.go
@@ -1,9 +1,28 @@
 package word_count
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func Test_word_count(t * testing.T) {
+func TestWordCount(t * testing.T) {
 
-	s := "This is a string. It has many words and many fullstops."
+	s := "This is a string. I like This string."
+
+	mpTest := WordCount(s)
+	mpTruth := make(map[string]int)
+
+	mpTruth["this"] = 2
+	mpTruth["is"] = 1
+	mpTruth["a"] = 1
+	mpTruth["string"] = 2
+	mpTruth["i"] = 1
+	mpTruth["like"] = 1
+
+	if !reflect.DeepEqual(mpTest, mpTruth) {
+
+		t.Errorf("expected map of %+v, but got map: %+v", mpTruth, mpTest)
+
+	}
 
 }
